65293a8c3288508704ba1bc929fd75bdf78263e5
sambooora <sambooora@Richie-PC.>
2025-06-19 12:02:08 +0700

    fix: node update vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'vercel'
+  },
     modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@vueuse/nuxt', '~/modules/stores.ts'],
     plugins: ['@/plugins/aos.client.ts'],
     css: ['~/assets/styles/plugin.css'],
