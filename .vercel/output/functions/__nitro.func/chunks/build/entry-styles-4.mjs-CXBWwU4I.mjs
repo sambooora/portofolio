d97af14265587cec16df9b9c1735ba129291ac78
sambooora <sambooora@Richie-PC.>
2025-10-22 18:00:55 +0700

    fix: test

--- .vercel/output/functions/__nitro.func/chunks/build/entry-styles-4.mjs-CXBWwU4I.mjs
+++
@@ -1,4 +0,0 @@
-const app_vue_vue_type_style_index_0_lang = '@import"https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@500;700;800&display=swap";';
-
-export { app_vue_vue_type_style_index_0_lang as a };
-//# sourceMappingURL=entry-styles-4.mjs-CXBWwU4I.mjs.map
