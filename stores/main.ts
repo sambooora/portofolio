1fc57678bb3c8870f04c9c0541063e90d80d32b2
sambooora <sambooora@Richie-PC.>
2026-07-08 13:58:14 +0700

    fix: root directory

--- stores/main.ts
+++ stores/main.ts
@@ -14,7 +14,7 @@ export const useMainStore = defineStore('app', {
       {
         id: '1',
         name: 'HabitFuel',
-        img: '/images/tmp/products/habitfuel-1.png',
+        img: '/images/habitfuel-1.png',
         description: `
           <p>This project a develop from scratch of Habit Tracker App. HabitFuel is a habit tracker app that helps users to track their habits and achieve their goals. In this project, My Role is Front End Developer and also Designer.
           I develope this Project with Stack :</p>
@@ -28,7 +28,7 @@ export const useMainStore = defineStore('app', {
       {
         id: '2',
         name: 'Pablo Restaurant',
-        img: '/images/tmp/products/pablo-1.png',
+        img: '/images/pablo-1.png',
         description: `
           <p>This project is a develop from scratch of Landing Page and Order Food App of Pablo Restaurant. Pablo Resto is a Resto under management of Grand Picasso Hotel in Jakarta, Indonesia. In this project, My Role is Front End Developer and also Designer.
             Is a website that aims to be a hotel landing page and promotion
@@ -44,7 +44,7 @@ export const useMainStore = defineStore('app', {
       {
         id: '3',
         name: 'Sam Wedding',
-        img: '/images/tmp/products/samwedding-1.png',
+        img: '/images/samwedding-1.png',
         description: `
          <p>This project is a develop from scratch of Landing Page of Sam Wedding. Sam Wedding is a wedding invitation website. In this project, My Role is Front End Developer and also Designer.
          I develope this Project with Stack :</p>
